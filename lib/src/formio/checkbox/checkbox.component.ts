7e5805590e9e424c18b8d0456e54c81faaa9e7c5
mattae <matthew.edor@gmail.com>
2024-09-12 14:16:24 +0100

    - Adjust checkbox value when rendered as radio, and corresponding display in PDF

--- lib/src/formio/checkbox/checkbox.component.ts
+++ lib/src/formio/checkbox/checkbox.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, effect } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { TranslocoModule } from '@jsverse/transloco';
 import { MatError, MatHint, MatSuffix } from '@angular/material/form-field';
@@ -8,22 +8,38 @@ import { MatTooltip } from '@angular/material/tooltip';
 import { MatRadioButton } from '@angular/material/radio';
 import { MaterialRadioComponent } from '../radio/radio.component';
 import _ from 'lodash';
+import { NgStyle } from '@angular/common';
 
 @Component({
     selector: 'mat-formio-checkbox',
     template: `
         @if (component) {
             @if (component.inputType === 'radio') {
-                <mat-radio-button
-                    [name]="component.name"
-                    value="{{ component.value }}"
-                    [checked]="isRadioChecked(component.value)"
-                    (click)="clicked()"
-                >
-                    @if (!component.labelIsHidden) {
-                        {{ component.label }}
-                    }
-                </mat-radio-button>
+                @if (component.inPdf) {
+                    <mat-checkbox
+                            value="{{ component.value }}"
+                            [checked]="isCheckboxChecked(component.value)"
+                            (click)="clicked()"
+                    >
+                        @if (!component.labelIsHidden) {
+                            {{ component.label }}
+                        }
+                    </mat-checkbox>
+                } @else {
+                    <mat-radio-button
+                            [ngStyle]="{
+                                zoom: '1.3'
+                            }"
+                            [name]="component.name"
+                            value="{{ component.value }}"
+                            [checked]="isRadioChecked(component.value)"
+                            (click)="clicked()"
+                    >
+                        @if (!component.labelIsHidden) {
+                            {{ component.label }}
+                        }
+                    </mat-radio-button>
+                }
             } @else {
                 <div class="flex flex-col">
                     <mat-checkbox (change)="onChange($event.checked)"
@@ -53,7 +69,8 @@ import _ from 'lodash';
         MatIcon,
         MatSuffix,
         MatTooltip,
-        MatRadioButton
+        MatRadioButton,
+        NgStyle
     ],
     standalone: true,
     changeDetection: ChangeDetectionStrategy.OnPush
@@ -68,10 +85,14 @@ export class MaterialCheckboxComponent extends MaterialRadioComponent {
         return value === this.instance.dataValue;
     }
 
+    isCheckboxChecked(value: any) {
+        return value === this.instance.dataValue;
+    }
+
     clicked(): void {
         this.control.setValue(this.component.value);
         _.set(this.instance.data, this.component.name, this.component.value)
-        this.instance.updateValue(null, {
+        this.instance.updateValue(this.component.value, {
             modified: true
         });
     }
